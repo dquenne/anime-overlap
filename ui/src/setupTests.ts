a120f573e0896abb1eb4b14bd570bde25f2f3aa4
rstoutamore <ryan.stoutamore@gmail.com>
2024-01-26 12:19:41 -0800

    Remove network calls from rendering `App` in tests
    Drop default create-react-app test on `App.tsx` and add a couple simple
    tests to `Birthday.tsx` that use a stub provider for `uqrl` to avoid
    network calls from the real web client-based provider in `App.tsx`.
    Also, add a fallback mock of `window.fetch` to ensure that no network
    calls ever slip through from the test runner.
    
    Co-authored-by: Dylan Quenneville <12236876+dquenne@users.noreply.github.com>

--- ui/src/setupTests.ts
+++ ui/src/setupTests.ts
@@ -1,7 +1,20 @@
-import { afterEach } from "vitest";
+import { afterEach, vi, MockInstance } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
+let mockedFetch: MockInstance<any>;
+
+beforeEach(() => {
+  mockedFetch = vi.spyOn(window, "fetch").mockImplementation((...args) => {
+    console.warn(
+      "`window.fetch` is not mocked for this test. Instead, test should render the component in a custom urql provider.",
+      ...args,
+    );
+    return Promise.reject(new Error("`window.fetch` is not mocked"));
+  });
+});
+
 afterEach(() => {
+  mockedFetch.mockReset();
   cleanup();
 });
