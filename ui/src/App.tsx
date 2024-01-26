b45b6e0d0b1795fc06695ac2459a369625207e14
rstoutamore <ryan.stoutamore@gmail.com>
2024-01-25 16:39:43 -0800

    Add proof-of-concept AniList GraphQL fetch to App.tsx
    Re-adding this, but within the starter Vite UI.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,33 +1,44 @@
 import { useState } from 'react'
+import { Client, Provider, cacheExchange, fetchExchange } from "urql"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+import { Birthday } from "./Birthday";
+
+const client = new Client({
+  url: "https://graphql.anilist.co",
+  exchanges: [cacheExchange, fetchExchange],
+});
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <Provider value={client}>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <Birthday />
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </Provider>
     </>
   )
 }
---
+++ ui/src/Birthday.tsx
@@ -0,0 +1,36 @@
+import { useQuery } from "urql";
+import { graphql } from "gql.tada";
+
+const BirthdayQuery = graphql(`
+  query BirthdayCharacters {
+    Character(isBirthday: true, sort: FAVOURITES_DESC) {
+      id
+      name {
+        full
+      }
+      media {
+        nodes {
+          title {
+            romaji
+          }
+        }
+      }
+    }
+  }
+`);
+
+export function Birthday() {
+  const [queryResult] = useQuery({ query: BirthdayQuery });
+  if (queryResult.fetching) {
+    return <>Loading birthday...</>;
+  }
+  if (queryResult.error) {
+    return <>Failed to fetch today's birthday character.</>;
+  }
+  return (
+    <>
+      Today is {queryResult.data?.Character?.name?.full}'s birthday (from "
+      {queryResult.data?.Character?.media?.nodes?.[0]?.title?.romaji}")
+    </>
+  );
+}
