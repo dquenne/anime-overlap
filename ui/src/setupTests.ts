c1a0124ffe32e87ce46fec278e69bddafc76e0de
rstoutamore <ryan.stoutamore@gmail.com>
2024-01-26 18:12:37 -0800

    Revert "Adds an assertion that mock fetch was not called"
    This reverts commit e67af72b43b8ec63ea9d7c26ec57c5fb51a20009.

--- ui/src/setupTests.ts
+++ ui/src/setupTests.ts
@@ -15,11 +15,6 @@ beforeEach(() => {
 });
 
 afterEach(() => {
-  // Required to enforce that fetch was not called, as React Query can gracefully handle fetch errors
-  expect(mockedFetch).not.toHaveBeenCalled();
-});
-
-afterEach(() => {
   mockedFetch.mockReset();
   cleanup();
 });
