af5e86fa6d125fbf2ffad74867c0d3b88b7667d7
Dylan Quenneville <dquenne@users.noreply.github.com>
2024-01-26 18:14:04 -0800

    Remove network calls from rendering `App` in tests (#17)
    Drop default create-react-app test on `App.tsx` and add a couple simple
    tests to `Birthday.tsx` that use a stub provider for `uqrl` to avoid
    network calls from the real web client-based provider in `App.tsx`.
    Also, add a fallback mock of `window.fetch` to ensure that no network
    calls ever slip through from the test runner.
    
    
    Co-authored-by: rstoutamore <ryan.stoutamore@gmail.com>
    Co-authored-by: Dylan Quenneville <12236876+dquenne@users.noreply.github.com>

---
+++ ui/src/Birthday.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "urql";
+import { Birthday } from "./Birthday";
+import { fromValue } from "wonka";
+
+describe("on load", () => {
+  const responseState = {
+    executeQuery: () =>
+      fromValue({
+        data: {
+          Character: {
+            name: { full: "Character A" },
+            media: { nodes: [{ title: { romaji: "Anime A" } }] },
+          },
+        },
+      }),
+  };
+
+  test("renders birthday character's name", () => {
+    render(
+      <Provider value={responseState}>
+        <Birthday />
+      </Provider>,
+    );
+    const linkElement = screen.getByText(/Character A's birthday/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("renders character's source media", () => {
+    render(
+      <Provider value={responseState}>
+        <Birthday />
+      </Provider>,
+    );
+    const linkElement = screen.getByText(/\(from "Anime A"\)/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
