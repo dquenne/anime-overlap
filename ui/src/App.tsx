849b535c6a86d87ed9087dc584141f94e8b7dd53
Dylan Quenneville <dquenne@gmail.com>
2024-02-01 15:09:23 -0800

    Drop unneeded Birthday.tsx

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,7 +4,6 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-import { Birthday } from "./Birthday";
 import { Staff } from "./Staff";
 
 const client = new Client({
